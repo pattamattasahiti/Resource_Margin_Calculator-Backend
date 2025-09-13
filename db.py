e98ee926eed1b5167be2ee15cb6b9c251b82d575
utsavsavanincompass <utsav.savani@ncompasbusiness.com>
2025-09-14 00:57:00 +0530

    - new files

--- db.py
+++ db.py
@@ -2,6 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Support multiple database types with fallback to SQLite
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL", "sqlite:///./resource_margin.db")
+
+# Configure engine based on database type
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
