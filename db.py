48e48487ea9ce98dd289349277cc0ae4d32d662d
Sahiti Pattamatta <spattamatta2025@fau.edu>
2025-09-12 20:32:56 -0400

    Switched SQLAlchemy adapter to PostgreSQL for Supabase

--- db.py
+++ db.py
@@ -1,7 +1,4 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost/resource_margin_db"
-
+import os
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")  # or paste the full string directly for local testing
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
